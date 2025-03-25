3908e1ed4f97776145d5f526f6c66ca09bbba211
Simon Van Casteren <simon.van.casteren@gmail.com>
2025-03-25 20:12:00 +0100

    Remove banner: screws with export

--- esbuild.ts
+++ esbuild.ts
@@ -8,19 +8,7 @@ esbuild
     platform: "node",
     outdir: "out",
     banner: {
-      js: `#!/usr/bin/env node
-import { createRequire as topLevelCreateRequire } from 'module';
-import { fileURLToPath as topLevelFileURLToPath } from 'url';
-import { dirname as topLevelDirname } from 'path';
-const __bundleRequire = topLevelCreateRequire(import.meta.url);
-const __bundleFilename = topLevelFileURLToPath(import.meta.url);
-const __bundleDirname = topLevelDirname(__bundleFilename);
-
-// Make these variables available globally for compatibility
-globalThis.require = __bundleRequire;
-globalThis.__filename = __bundleFilename;
-globalThis.__dirname = __bundleDirname;
-`,
+      js: `#!/usr/bin/env node`,
     },
     external: ["pg-native"],
   })

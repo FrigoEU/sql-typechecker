db8dfdc25c6f6bdcd317f4311c013eb39a43b0d2
Simon Van Casteren <simon.van.casteren@gmail.com>
2025-03-25 14:14:35 +0100

    fix(added banner)

--- esbuild.ts
+++ esbuild.ts
@@ -7,7 +7,23 @@ esbuild
     sourcemap: true,
     platform: "node",
     outdir: "out",
-    banner: { js: "#!/usr/bin/env node" },
+    banner: {
+      js: `#!/usr/bin/env node
+import { createRequire as topLevelCreateRequire } from 'module';
+import { fileURLToPath as topLevelFileURLToPath } from 'url';
+import { dirname as topLevelDirname, join as topLevelJoin } from 'path';
+const __bundleRequire = topLevelCreateRequire(import.meta.url);
+const __bundleFilename = topLevelFileURLToPath(import.meta.url);
+const __bundleDirname = topLevelDirname(__bundleFilename);
+const __bundleJoin = topLevelJoin;
+
+// Make these variables available globally for compatibility
+globalThis.require = __bundleRequire;
+globalThis.__filename = __bundleFilename;
+globalThis.__dirname = __bundleDirname;
+globalThis.join = __bundleJoin;
+`,
+    },
     external: ["pg-native"],
   })
   .catch(() => process.exit(1));

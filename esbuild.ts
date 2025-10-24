ac3ff8435e64e4473bdd395a6103b19549f0f87b
Simon Van Casteren <simon.van.casteren@gmail.com>
2025-10-24 19:54:02 +0200

    move to esm

--- esbuild.ts
+++ esbuild.ts
@@ -1,15 +1,18 @@
 import esbuild from "esbuild";
+import * as fs from "node:fs/promises";
 
 esbuild
   .build({
-    entryPoints: ["src/cli.ts", "src/typeparsers.ts"],
+    entryPoints: ["src/cli.ts"],
     bundle: true,
     sourcemap: true,
     platform: "node",
+    format: "esm",
     outdir: "out",
     banner: {
       js: `#!/usr/bin/env node`,
     },
     external: ["pg-native"],
   })
+  .then(() => fs.copyFile("src/typeparsers.ts", "out/typeparsers.ts"))
   .catch(() => process.exit(1));
